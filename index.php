e861600fa1ab5accd3eb4566df88e69a1008cca9
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-14 14:52:49 +0100

    Stampata in pagina la lunghezza del paragrafo originale

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     $paragraph = "Pippo (Goofy, in precedenza Dippy Dawg e Dippy the Goof) è un personaggio immaginario dei cartoni animati e dei fumetti della Disney, ideato nel 1932 da Pinto Colvig e dall'animatore Johnny Cannon come comprimario di Topolino in un cortometraggio, ma viene caratterizzato definitivamente dall'animatore Art Babbitt nel 1935 e parallelamente esordisce nei fumetti realizzati da Floyd Gottfredson che definisce ulteriormente il personaggio dandogli spessore come spalla principale di Topolino. È apparso in centinaia di cartoni animati come protagonista o comprimario e in migliaia di albi a fumetti realizzati in vari paesi del mondo.";
+    $paragraph_length = strlen($paragraph);
     $censored_word = $_GET["badword"];
     $censorship = "***";
     $censored_paragraph = str_replace($censored_word, $censorship, $paragraph);
@@ -18,7 +19,7 @@
         </p>
         <h1>
             <?php
-                echo "Badword: " . $censored_word;
+                echo "Uncensored paragraph's length: " . $paragraph_length;
             ?>
         </h1>
     </body>
