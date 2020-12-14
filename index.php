d5e3047225af2ee3d463d62e7dee3919840fe567
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-14 14:36:13 +0100

    Creata una variabile per leggere da un parametro GET della querystring una "badword" e stamparla in pagina

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     $paragraph = "Pippo (Goofy, in precedenza Dippy Dawg e Dippy the Goof) è un personaggio immaginario dei cartoni animati e dei fumetti della Disney, ideato nel 1932 da Pinto Colvig e dall'animatore Johnny Cannon come comprimario di Topolino in un cortometraggio, ma viene caratterizzato definitivamente dall'animatore Art Babbitt nel 1935 e parallelamente esordisce nei fumetti realizzati da Floyd Gottfredson che definisce ulteriormente il personaggio dandogli spessore come spalla principale di Topolino. È apparso in centinaia di cartoni animati come protagonista o comprimario e in migliaia di albi a fumetti realizzati in vari paesi del mondo.";
+    $censored_word = $_GET["badword"];
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -13,5 +14,10 @@
                 echo $paragraph;
             ?>
         </p>
+        <h1>
+            <?php
+                echo "Badword: " . $censored_word;
+            ?>
+        </h1>
     </body>
 </html>
