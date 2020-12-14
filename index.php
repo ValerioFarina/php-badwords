370e34c2b503891ac5b6a0b52b05d4a8d34ed6d9
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-14 14:46:28 +0100

    Sostituito all'interno del paragrafo tutte le occorrenze della badword e stampato in pagina il paragrafo censurato

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
     $paragraph = "Pippo (Goofy, in precedenza Dippy Dawg e Dippy the Goof) è un personaggio immaginario dei cartoni animati e dei fumetti della Disney, ideato nel 1932 da Pinto Colvig e dall'animatore Johnny Cannon come comprimario di Topolino in un cortometraggio, ma viene caratterizzato definitivamente dall'animatore Art Babbitt nel 1935 e parallelamente esordisce nei fumetti realizzati da Floyd Gottfredson che definisce ulteriormente il personaggio dandogli spessore come spalla principale di Topolino. È apparso in centinaia di cartoni animati come protagonista o comprimario e in migliaia di albi a fumetti realizzati in vari paesi del mondo.";
     $censored_word = $_GET["badword"];
+    $censorship = "***";
+    $censored_paragraph = str_replace($censored_word, $censorship, $paragraph);
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -11,7 +13,7 @@
     <body>
         <p>
             <?php
-                echo $paragraph;
+                echo $censored_paragraph;
             ?>
         </p>
         <h1>
