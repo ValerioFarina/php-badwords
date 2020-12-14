5afe6f32e9bdc6a1498564c09623b661b4b24180
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-14 14:29:29 +0100

    Creata una variabile paragrafo  e stampata in pagina

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    $paragraph = "Pippo (Goofy, in precedenza Dippy Dawg e Dippy the Goof) è un personaggio immaginario dei cartoni animati e dei fumetti della Disney, ideato nel 1932 da Pinto Colvig e dall'animatore Johnny Cannon come comprimario di Topolino in un cortometraggio, ma viene caratterizzato definitivamente dall'animatore Art Babbitt nel 1935 e parallelamente esordisce nei fumetti realizzati da Floyd Gottfredson che definisce ulteriormente il personaggio dandogli spessore come spalla principale di Topolino. È apparso in centinaia di cartoni animati come protagonista o comprimario e in migliaia di albi a fumetti realizzati in vari paesi del mondo.";
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Bad Words</title>
+    </head>
+    <body>
+        <p>
+            <?php
+                echo $paragraph;
+            ?>
+        </p>
+    </body>
+</html>
